ee5176f24312d3c2e0ab0ff25831f58b3f0172b5
atp <atp1798@gmail.com>
2023-03-24 09:20:41 -0400

    update

--- py_flask/app/chat_server.py
+++ py_flask/app/chat_server.py
@@ -6,6 +6,9 @@ import openai
 import os
 from config import get_config
 from bot.bot_factory import create_bot
+import xmltodict
+import dicttoxml
+import time
 
 
 class ChatServer:
@@ -105,6 +108,66 @@ class ChatServer:
             # 返回结果到客户端
             return jsonify({"code": 200, "msg": "success", "data": result})
 
+        @self._app.route("/openai/session/wechat/chat-completion", methods=["GET"])
+        def do_wechat_check():
+            logger.info("echostr +++ {}".format(request.args.get("echostr")))
+            #return jsonify({"code": 200, "msg": "success", "data": request.args.get("echostr")})
+            return request.args.get("echostr"), 200
+
+        @self._app.route("/openai/session/wechat/chat-completion", methods=["POST"])
+        def session_wechat_chat_completion():
+            if self._debug_mode:
+                debug_request(request)
+
+            #parameter constant
+            QUERY = "Content"
+            SESSION_ID = "FromUserName"
+
+            logger.info("request={} headers={} reqpath={} args={} data={} form={}".format(request, request.headers, request.path, request.args, request.data, request.form))
+            request_json = xmltodict.parse(request.data)['xml']
+            logger.info("request_json={}".format(request_json))
+
+            if len(request_json) == 0:
+                return jsonify({"code": 301, "msg": "empty request"})
+            if QUERY not in request_json or not isinstance(
+                    request_json[QUERY], str):
+                return jsonify({"code": 301, "msg": "empty query"})
+            if SESSION_ID not in request_json or not isinstance(
+                    request_json[SESSION_ID], str):
+                return jsonify({"code": 301, "msg": "empty session id"})
+
+            session_id = request_json["FromUserName"] 
+            query = request_json["Content"]
+
+            context = dict()
+            context['session_id'] = session_id
+
+            response = None
+            result = ""
+            try:
+                response = self._bot.reply(query, context)
+                # 从响应中获取结果
+                result = response
+            except Exception:
+                if self._debug_mode:
+                    print(response)
+                return jsonify({"code": 302, "msg": "internal error"})
+
+            res={}
+            res["ToUserName"] = request_json["FromUserName"]
+            res["FromUserName"] = request_json["FromUserName"]
+            res["CreateTime"] = int(time.time())
+            res["MsgType"] = "transfer_customer_service"
+            res["Content"] = result
+            xml_res = {}
+            xml_res["xml"] = res
+            logger.info("response={} xml={}".format(result, dict2xml(xml_res)))
+            
+            # 返回结果到客户端
+            return jsonify({"code": 200, "msg": "success", "data": result})
+
+
+	
         @self._app.route("/openai/session/chat-completion", methods=["POST"])
         def session_chat_completion():
             if self._debug_mode:
