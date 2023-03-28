9768b66f185fbff42b718b95f5511d01286cab07
silentreverie <123400544@qq.com>
2023-03-28 12:15:14 +0800

    update

--- py_flask/script/start_for_debug.sh
+++ py_flask/script/start_for_debug.sh
@@ -2,5 +2,6 @@
  ps aux|grep run.py|awk '{print $2}'| xargs kill -9
 pwd
 #source script/export_api_token.sh
-nohup python run.py 2>&1 & 
+touch nohup.out
+nohup python3 run.py 2>&1 & 
 tail -f nohup.out
