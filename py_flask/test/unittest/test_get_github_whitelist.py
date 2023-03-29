8b762c69f88e0b7876f7607332539665506150a4
silentreverie <123400544@qq.com>
2023-03-29 15:38:06 +0800

    update

---
+++ py_flask/test/unittest/test_get_github_whitelist.py
@@ -0,0 +1,15 @@
+import requests
+import json
+import base64
+
+
+def get_whitelist_from_github(url="https://api.github.com/repos/atp798/chatbot/contents/py_flask/common/wxmp_whitelist.json")
+    try:
+        response = requests.get(url)
+        json_data = json.loads(response.content)
+        content = json_data['content']
+        decoded_content = base64.b64decode(content).decode('utf-8')
+        json_obj = json.loads(decoded_content)
+        return json_obj
+    except Exception as e:
+        return None
