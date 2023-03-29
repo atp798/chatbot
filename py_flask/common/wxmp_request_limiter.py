7c32c2ab054a3096f3d7525bea9049c9e67859c1
silentreverie <123400544@qq.com>
2023-03-29 15:34:34 +0800

    get white list from github

--- py_flask/common/wxmp_request_limiter.py
+++ py_flask/common/wxmp_request_limiter.py
@@ -3,6 +3,7 @@ import time,datetime
 import sys
 from common.log import logger
 import json
+import requests
 
 
 class WxmpVipLimit:
@@ -16,20 +17,9 @@ class WxmpVipLimit:
 
 class WxmpRequestLimiter:
     def __init__(self, path="./common/wxmp_whitelist.json"):
-        confdict = None
-        with open(path, mode='r', encoding='utf-8') as f:
-             confdict = json.loads(f.read())
-        if (confdict is None):
-            logger.error("cannot open vip list, exit")
-            sys.exit(-1)
-
-        #把字典倒过来
-        tmp_dict = {}
-        for key, value in confdict.items():
-            for v in value:
-                tmp_dict[v] = key
-        self.openid_dict = tmp_dict 
-        
+        self.last_update_timestamp = None
+        self.confdict = {}
+        self.openid_dict = {}
         ##vip0: 普通vip, 每天10条, 默认等级
         ##vip1: 普通vip, 每天10条, 默认等级, 留一个做扩展
         ##vip2: 无限量访问
@@ -42,6 +32,32 @@ class WxmpRequestLimiter:
             "vip_level_3": WxmpVipLimit(1000, 1000),
             "vip_level_forever": WxmpVipLimit(1000, 1000),
         }
+        return
+
+    #同步更新，粗暴实现，可以考虑异步
+    def update_whitelist(self):
+        if self.last_update_timestamp is None or time.time() - self.last_update_timestamp > 60:
+            self.confdict = self.get_whitelist_from_github()
+            #把字典倒过来
+            tmp_dict = {}
+            for key, value in confdict.items():
+                for v in value:
+                    tmp_dict[v] = key
+            self.openid_dict = tmp_dict 
+
+    def get_whitelist_from_github(self, url="https://api.github.com/repos/atp798/chatbot/contents/py_flask/common/wxmp_whitelist.json"):
+        try:
+            response = requests.get(url)
+            json_data = json.loads(response.content)
+            content = json_data['content']
+            decoded_content = base64.b64decode(content).decode('utf-8')
+            json_obj = json.loads(decoded_content)
+            logger.info("update remote whitelist succ!!")
+            logger.info("".format(json_obj))
+            return json_obj
+        except Exception as e:
+            logger.info("update remote whitelist error!!")
+            return None 
 
     def get_vip_level(self, openid):
         return self.openid_dict.get(openid, "vip_level_0")
@@ -50,6 +66,9 @@ class WxmpRequestLimiter:
         return self.vip_limit_dict.get(self.get_vip_level(openid))
     
     def do_limit(self, openid, session, msgtype):
+        #每60s更新一下数据表
+        self.update_whitelist()
+
         #这个方法实现的比较挫，每次都遍历，但考虑到用户少，也没啥了
         # 设置时区
         tz_offset = 8  # 东八区
