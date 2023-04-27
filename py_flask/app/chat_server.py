6ce2ae9dd66b985b95f9308d0353910d738c26f1
silentreverie <123400544@qq.com>
2023-04-28 00:53:30 +0800

    udpate

--- py_flask/app/chat_server.py
+++ py_flask/app/chat_server.py
@@ -170,8 +170,10 @@ class ChatServer:
                 context['session_id'] = session_id
                 context['type'] = "TEXT_ONCE" #text without session
                 context['loginfo'] = loginfo
-                response = self._bot.reply('Tell me: 1. If the content below is a drawing request; 2. If the content is appropriate for a 15 years old. Answer me with word in options [YES,NO,UNCERTAIN] and use format [xx,xx]:' + query, context)
-                res = response.strip().split(' ')
+                response = self._bot.reply(
+                    'I will show you a string below, tell me 1. If the content is a drawing request 2. If the content is appropriate for 15 years old. You must give me just 2 words answer, and each word must be in options [YES,NO,UNCERTAIN]:'
+                    + query, context)
+                res = response.strip().split(', ')
                 msgtype = "TEXT"
                 if len(res) >= 2:
                     msgtype = "IMAGE_SD" if ("YES" in res[0]) and ("NO" not in res[1]) else msgtype
