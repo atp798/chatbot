b5e94744f99a37ba5dde85c360d80ac052a0e313
silentreverie <123400544@qq.com>
2023-05-01 07:34:09 +0800

    update

--- py_flask/bot/chatgpt/chat_gpt_bot.py
+++ py_flask/bot/chatgpt/chat_gpt_bot.py
@@ -84,8 +84,8 @@ class ChatGPTBot(Bot):
                 response = openai.ChatCompletion.create(
                 model= get_config().gpt_model,  # 对话模型的名称
                 messages=session,
-                temperature=0.2,  # 值在[0,1]之间，越大表示回复越具有不确定性
-                top_p=1,
+                temperature=1,  # 值在[0,1]之间，越大表示回复越具有不确定性
+                top_p=0.4,
                 frequency_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 presence_penalty=0.0,  # [-2,2]之间，该值越大则更倾向于产生不同的内容
                 )
