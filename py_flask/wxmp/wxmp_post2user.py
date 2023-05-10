778bef68cb013feacce3f2589c13cb0aed26e5e8
silentreverie <123400544@qq.com>
2023-05-10 15:06:37 +0800

    update

--- py_flask/wxmp/wxmp_post2user.py
+++ py_flask/wxmp/wxmp_post2user.py
@@ -76,7 +76,7 @@ def post_img_respons2wxmp_SD(image_base64=None, touser=None, retry=0):
     try:
         local_path = '/var/tmp/' + touser + '_' + str(int(time.time() * 1000)) + '.png'
         with open(local_path, "wb") as f:
-            f.write(io.BytesIO(base64.b64decode(image_base64)))
+            f.write(base64.b64decode(image_base64))
         media_id = img_upload(local_path)
         delete_image(local_path)
     except Exception as e:
