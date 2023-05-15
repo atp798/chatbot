8bce35a297d31e6ce51a979e5f2dc4af6b11861b
silentreverie <123400544@qq.com>
2023-05-15 14:10:31 +0800

    update

--- py_flask/common/session_new.py
+++ py_flask/common/session_new.py
@@ -68,7 +68,8 @@ class SessionManager(object):
         if session_id not in self.sessions:
             self.sessions[session_id] = self.sessioncls(session_id, system_prompt, **self.session_args)
         elif system_prompt is not None:  # 如果有新的system_prompt，更新并重置session
-            self.sessions[session_id].set_system_prompt(system_prompt)
+            pass
+            #self.sessions[session_id].set_system_prompt(system_prompt)
         session = self.sessions[session_id]
         return session
 
