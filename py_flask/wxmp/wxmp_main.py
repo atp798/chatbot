74b05779cd40d3d4876a7191d8dc5764885258ee
silentreverie <123400544@qq.com>
2023-05-15 15:39:42 +0800

    update

--- py_flask/wxmp/wxmp_main.py
+++ py_flask/wxmp/wxmp_main.py
@@ -60,7 +60,8 @@ def process_wxmp_request(request_json, bot):
             msg_type = "IMAGE_SD" if ("YES" in res[0]) and (not "NO" in res[1]) else "TEXT"
         loginfo.append("res={}, msgtype={}".format(res, msg_type))
         if msg_type != "IMAGE_SD": #说明不符合绘画需求
-            post_respons2wxmp("您的绘画请求中包含不适合青少年的内容，将以文字问答形式回答您的问题。", toUserName)
+            post_respons2wxmp("您的绘画请求中可能包含不适合青少年的内容，请重新提问。", toUserName)
+            return
 
     logger.info('begin process, {}'.format('; '.join(loginfo)))
     context = {}
