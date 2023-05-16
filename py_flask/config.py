e59b1118d66bd11654a525f710709c60f16ff34a
silentreverie <123400544@qq.com>
2023-05-16 10:19:13 +0800

    update

--- py_flask/config.py
+++ py_flask/config.py
@@ -93,6 +93,7 @@ class ConfigParser:
         self.open_wxmp = self._json_config.get("open_wxmp", True)
         self.max_token_size = self._json_config.get("max_token_size", 2048)
         self.google_search_api_key = ""
+        self.google_search_cx = ""
 
 
 
