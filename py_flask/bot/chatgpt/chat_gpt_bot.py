06a340798a9cca62416478b03ecff26136c7f802
silentreverie <123400544@qq.com>
2023-05-18 09:32:23 +0800

    update

--- py_flask/bot/chatgpt/chat_gpt_bot.py
+++ py_flask/bot/chatgpt/chat_gpt_bot.py
@@ -65,16 +65,16 @@ class ChatGPTBot(Bot):
             reply_content = self.reply_text(session.messages, session_id, retry_count=0, strict_completion=True)
             if reply_content["completion_tokens"] > 0:
                 self._session.session_reply(reply_content["content"], session_id, reply_content["total_tokens"], 1024)
-        elif msgtype == const.TEXT:
-            reply_content = self.reply_text(session.messages, session_id, 0)
-            if reply_content["completion_tokens"] > 0:
-                self._session.session_reply(reply_content["content"], session_id, reply_content["total_tokens"])
         elif msgtype == const.IMAGE:
             reply_content = self.reply_image(query, 0)
         elif msgtype == const.IMAGE_RAW:
             reply_content = self.reply_image_rawdata(query)
         elif msgtype == const.IMAGE_SD:
-            reply_content = {"content": self.request_sd_image(query, context)} 
+            reply_content = {"content": self.request_sd_image(query, context)}
+        else: #msgtype == const.TEXT:
+            reply_content = self.reply_text(session.messages, session_id, 0)
+            if reply_content["completion_tokens"] > 0:
+                self._session.session_reply(reply_content["content"], session_id, reply_content["total_tokens"])
 
         tdiff = time.time() - btime
         loginfo.append("openai_query=[{}], openai_msgtype={}, openai_time={}".format(query, msgtype, int(tdiff * 1000)))
