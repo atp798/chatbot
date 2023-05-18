8d06ff8f0898b26f64ae4b2142e5c9d545044f88
silentreverie <123400544@qq.com>
2023-05-18 11:21:26 +0800

    update

--- py_flask/common/utils.py
+++ py_flask/common/utils.py
@@ -1,6 +1,8 @@
 import re
 from common.log import logger
 import requests
+from thefuzz import fuzz
+from thefuzz import process
 
 def is_chinese(string):
     """
@@ -16,7 +18,15 @@ def get_google_search_content(query):
         headers = {'Content-Type': 'application/json'}
         data = {'query': query}
         response = requests.post(url, headers=headers, json=data)
-        logger.info("google search res json:{}".format(response.json()))
+        data = response.json['data']
+        titles_dict = {d['title']: d for d in data}
+        titles = titles_dict.keys()
+        titles_scores = [fuzz.partial_token_set_ratio(t) for t in titles]
+        logger.info("google search titles={} scores={}".format(titles, titles_scores))
+        max_score = max(titles_scores)
+        best_title = titles[ [i for i in range(0, len(titles_scores)) if titles_scores[i] == max_score][0] ]
+        logger.info("google search best_title={} content={}".format(best_title, titles_dict[best_title]))
+        #logger.info("google search res json:{}".format(response.json()))
         return response.json
     except Exception as e:
         logger.exception(e)
